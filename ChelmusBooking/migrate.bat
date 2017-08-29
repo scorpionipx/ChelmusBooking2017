506e92b0da6c319244a2f4d586ec824177be5cbb
DESKTOP-PE8APUQ\ScorpionIPX <scorpionipx@gmail.com>
2017-08-30 00:10:57 +0300

    Created bat files for running the server and making migrations

---
+++ ChelmusBooking/migrate.bat
@@ -0,0 +1,4 @@
+color b
+@echo off
+cls
+cmd /k "cd /d D:\Documents\Python\Django\ChelmusBooking2017\chelmus_booking_virtualenv\Scripts & activate & cd /d    D:\Documents\Python\Django\ChelmusBooking2017\ChelmusBooking & python manage.py migrate"
---
+++ ChelmusBooking/run_server_default.bat
@@ -0,0 +1,4 @@
+color a
+@echo off
+cls
+cmd /k "cd /d D:\Documents\Python\Django\ChelmusBooking2017\chelmus_booking_virtualenv\Scripts & activate & cd /d    D:\Documents\Python\Django\ChelmusBooking2017\ChelmusBooking & python manage.py runserver"
