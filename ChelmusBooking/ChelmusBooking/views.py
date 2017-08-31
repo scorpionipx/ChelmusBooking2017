79d4cd39ff43dd0b15cd3c7798166b33b7dd461f
DESKTOP-PE8APUQ\ScorpionIPX <scorpionipx@gmail.com>
2017-08-31 15:17:03 +0300

    Created register and login methods and templates

--- ChelmusBooking/ChelmusBooking/views.py
+++ ChelmusBooking/ChelmusBooking/views.py
@@ -1,7 +1,10 @@
 from django.views import generic
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.core.urlresolvers import reverse_lazy
 
 from .forms import RegistrationForm
+from .forms import LoginForm
 
 
 class HomePageView(generic.TemplateView):
@@ -13,6 +16,22 @@ class SignUpView(generic.CreateView):
     model = User
     template_name = 'accounts/signup.html'
 
+
+class LoginView(generic.FormView):
+    form_class = LoginForm
+    success_url = reverse_lazy('home')
+    template_name = 'accounts/login.html'
+
+    def form_valid(self, form):
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None and user.is_active:
+            login(self.request, user)
+            return super(LoginView, self).form_valid(form)
+        else:
+            return self.form_invalid(form)
 
 
 
